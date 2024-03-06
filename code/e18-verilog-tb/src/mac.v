10f62e573620ea0bbeced9121555a0ead3d7fc47
Saadia Jameel <saadiajameel@Saadias-MBP.cedyn.pdn.ac.lk>
2024-03-06 16:46:45 +0530

    mac unit added

--- code/e18-verilog-tb/src/mac.v
+++ code/e18-verilog-tb/src/mac.v
@@ -1,22 +1,92 @@
 // MAC unit can process 4 input spikes together
+// 4 spike inputs 
+// 4 weights corresponding to the synapse
 
-module mac():
+module mac(input wire [3:0] spike_in, input wire [127:0] weight, output reg [127:0] mult_ans);
 
-    input wire [3:0] beta;
-    input wire [7:0] potential;
-    output reg [7:0] mult_ans;
+    reg [127:0] mask;
 
     always @(*) begin 
-        case (beta) 
+        case (spike_in) 
             4'd0:   mult_ans <= 0;                  // No spikes in any of the 4 branches.
-            4'd1;   mult_ans <= potential >> 3; 
-            4'd2:   mult_ans <= potential >> 2;
-            4'd3:   mult_ans <= (potential >> 2) + (potential >> 3);
-            4'd4:   mult_ans <= (potential >> 1); 
-            4'd5:   mult_ans <= (potential >> 1) + (potential >> 3);
-            4'd6:   mult_ans <= (potential >> 1) + (potential >> 2);
-            4'd7:   mult_ans <= (potential >> 1) + (potential >> 2) + (potential >> 3);
-            4'd8:   mult_ans <= potential;
+            4'd1: begin
+                mask = 128'b0;
+                mask[31:0] = 32'd4294967295;   
+                mult_ans <= weight & mask; 
+            end
+            4'd2: begin
+                mask = 128'b0;
+                mask[63:32] = 32'd4294967295;   
+                mult_ans <= weight & mask; 
+            end
+            4'd3: begin
+                mask = 128'b0;
+                mask[63:0] = 32'd4294967295;   
+                mult_ans <= weight & mask; 
+            end
+            4'd4: begin
+                mask = 128'b0;
+                mask[95:64] = 32'd4294967295;   
+                mult_ans <= weight & mask; 
+            end
+            4'd5: begin
+                mask = 128'b0;
+                mask[31:0] = 32'd4294967295; 
+                mask[95:64] = 32'd4294967295;   
+                mult_ans <= weight & mask; 
+            end
+            4'd6: begin
+                mask = 128'b0;
+                mask[95:32] = 32'd4294967295;   
+                mult_ans <= weight & mask; 
+            end
+            4'd7: begin
+                mask = 128'b0;
+                mask[95:0] = 32'd4294967295;   
+                mult_ans <= weight & mask; 
+            end
+            4'd8: begin
+                mask = 128'b0;
+                mask[127:96] = 32'd4294967295;   
+                mult_ans <= weight & mask; 
+            end
+            4'd9: begin
+                mask = 128'b0;
+                mask[127:96] = 32'd4294967295;  
+                mask[31:0] = 32'd4294967295;
+                mult_ans <= weight & mask; 
+            end
+            4'd10: begin
+                mask = 128'b0;
+                mask[127:96] = 32'd4294967295;  
+                mask[63:32] = 32'd4294967295;
+                mult_ans <= weight & mask; 
+            end
+            4'd11: begin
+                mask = 128'b0;
+                mask[127:96] = 32'd4294967295;  
+                mask[63:0] = 32'd4294967295;
+                mult_ans <= weight & mask; 
+            end
+            4'd12: begin
+                mask = 128'b0;
+                mask[127:64] = 32'd4294967295;
+                mult_ans <= weight & mask; 
+            end
+            4'd13: begin
+                mask = 128'b0;
+                mask[127:96] = 32'd4294967295;  
+                mask[31:0] = 32'd4294967295;
+                mult_ans <= weight & mask; 
+            end
+            4'd14: begin
+                mask = 128'b0;
+                mask[127:32] = 32'd4294967295;
+                mult_ans <= weight & mask; 
+            end
+            4'd15: begin
+                mult_ans <= weight;
+            end
             default:    mult_ans <= 4'bx;
         endcase
     end
