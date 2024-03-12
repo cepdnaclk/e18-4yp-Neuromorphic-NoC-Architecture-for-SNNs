59c56300bd076859b272816ad70f594bedf390b0
Saadia Jameel <saadiajameel@Saadias-MacBook-Pro.local>
2024-03-12 12:50:39 +0530

    files for needed units created

---
+++ code/e18-verilog-tb/src/adder.v
@@ -0,0 +1,13 @@
+module adder(
+    input wire [31:0] weight, 
+    input wire [31:0] decayed_potential, 
+    output reg [31:0] potential, 
+    output reg spike);
+
+    // Addition and comparison
+    
+     
+
+
+
+endmodule
---
+++ code/e18-verilog-tb/src/reset.v
@@ -0,0 +1,7 @@
+module (input wire [31:0] adder_potential, 
+    input wire spiked, 
+    output reg [31:0] potential_to_mem);
+
+    // Reset if needed only, if not direct write
+
+endmodule
