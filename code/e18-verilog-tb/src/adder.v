1b85fb3fdd1c6c082bf7e802b3cb8c14f03cad60
Saadia Jameel <saadiajameel@Saadias-MacBook-Pro.local>
2024-03-12 16:30:41 +0530

    reset, mac, adder basics done

--- code/e18-verilog-tb/src/adder.v
+++ code/e18-verilog-tb/src/adder.v
@@ -1,4 +1,5 @@
 module adder(
+    input wire [31:0] v_threshold,
     input wire [31:0] weight, 
     input wire [31:0] decayed_potential, 
     output reg [31:0] potential, 
@@ -6,8 +7,11 @@ module adder(
 
     // Addition and comparison
     
-     
+    // First add the connection inputs to the decayed
+    assign potential = weight + decayed_potential;
 
+    // Compare to see if spiked
+    assign spike = potential >= v_threshold;     
 
 
 endmodule
--- code/e18-verilog-tb/src/reset.v
+++ code/e18-verilog-tb/src/reset.v
@@ -1,7 +1,15 @@
 module (input wire [31:0] adder_potential, 
     input wire spiked, 
+    input wire [31:0] v_threshold,
     output reg [31:0] potential_to_mem);
 
     // Reset if needed only, if not direct write
+    if (spiked) begin
+        // Reset the potential according to the model
+        // V <- V - Vth
+        assign potential_to_mem = adder_potential - v_threshold;
+
+    end else
+        assign potential_to_mem = potential;
 
 endmodule
