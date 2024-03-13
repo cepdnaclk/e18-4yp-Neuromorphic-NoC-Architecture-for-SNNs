412993a5d1b21f4f93065ccb6793dc297835469a
Thamish99 <e18379@eng.pdn.ac.lk>
2024-03-13 22:31:21 +0530

    potential decay basic done

--- code/e18-verilog-tb/src/potential_decay.v
+++ code/e18-verilog-tb/src/potential_decay.v
@@ -1,21 +1,47 @@
 `timescale 1ns/100ps
+//Potential decay for divinding by 1, 2, 4, and 8 only
+
 
 module potential_decay(
 
-    input wire CLK,                         //input clock
-    input reg[31:0] delay_rate, membrane_potential,                //input delay rate, membrane_potential
+    input wire CLK,                             //input clock
+    input wire[2:0] decay_rate,                 //input decay rate,
+    input wire[31:0] membrane_potential,        // membrane_potential
+    output reg[31:0] output_potential_decay     //output of the new potential value after decay
+    );   
     
-    output reg[31:0] output_potential_decay);   //output of the new potential value after decay
-
-    wire sign = membrane_potential[31];     //sign of the potential value
-    wire [7:0] exponent = membrane_potential[30:23];    //get exponent of membrane potential value 
-    wire [22:0] mantissa = membrane_potential[22:0];    //get mantissa from membrane potential value
-    wire [7:0] adjusted_exponent;
+    reg [1:0] sign;        //sign of the potential value
+    reg [7:0] exponent;    //get exponent of membrane potential value 
+    reg [22:0] mantissa;   //get mantissa from membrane potential value
+    reg [7:0] adjusted_exponent;   //variable to assign new exponent value
 
     always @(*) begin
-        adjusted_exponent = exponent-8'b1;
+        sign = membrane_potential[31];     
+        exponent = membrane_potential[30:23];    
+        mantissa = membrane_potential[22:0];    
+        
+        case (decay_rate)
+
+            3'd0: begin
+                adjusted_exponent = exponent;
+            end
+
+            3'd1: begin
+                adjusted_exponent = exponent-8'd1;
+            end
+
+            3'd2: begin
+                adjusted_exponent = exponent-8'd2;
+            end
+
+            3'd3: begin
+                adjusted_exponent = exponent-8'd3;
+            end
+
+            default: adjusted_exponent = exponent;
+        endcase
+
+        output_potential_decay = {sign, adjusted_exponent, mantissa};
     end
 
-    output_potential_decay = {sign, adjusted_exponent, mantissa};
-    
 endmodule
