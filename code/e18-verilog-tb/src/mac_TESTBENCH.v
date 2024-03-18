b083db6d02ee8e02f8ec979e97c7ec4f5b23a7d7
Saadia Jameel <saadiajameel@Saadias-MacBook-Pro.local>
2024-03-18 22:57:00 +0530

    floating point unit added to accumulator

--- code/e18-verilog-tb/src/mac_TESTBENCH.v
+++ code/e18-verilog-tb/src/mac_TESTBENCH.v
@@ -25,36 +25,31 @@ module testbench;
     begin 
 
         CLK = 1'b0;
+        // 0.1, 2, 1.2, 3
+        // 00111101110011001100110011001101
+        // 01000000000000000000000000000000
+        // 00111111100110011001100110011010
+        // 01000000010000000000000000000000
+        weights_in = 128'd82146290175160474081457091226028212224;         // IEEE format 
 
         #3
-        spike_in  = 4'd0;
-        weights_in = 128'd18321983;
 
+        spike_in  = 4'd0;
         #8
 
         spike_in  = 4'd1;
-        weights_in = 128'd18321983;
-
         #8
 
         spike_in  = 4'd0;
-        weights_in = 128'd18321983;
-
         #8
 
         spike_in  = 4'd1;
-        weights_in = 128'd18321983;
-
         #8
 
         spike_in  = 4'd1;
-        weights_in = 128'd18321983;
-
         #8
 
         spike_in  = 4'd0;
-        weights_in = 128'd18321983;
-
         #8
 
         $finish;
